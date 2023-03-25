75a4796a2349fb341f2f2d26654d80c638ca72b1
phuchtce150394 <phuchtce150394@fpt.edu.vn>
2023-03-25 18:32:48 +0700

    Update serializer

--- predictor/serializers.py
+++ predictor/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
-from .models import PredictionModel
+from .models import PredictionModel, PredictionResult, PredictionResultItem
 
+
+class PredictionResultItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PredictionResultItem
+        fields = '__all__'
+
+class PredictionResultSerializer(serializers.ModelSerializer):
+
+    items = PredictionResultItemSerializer(many=True)
+
+    class Meta:
+        model = PredictionResult
+        fields = '__all__'
 
 class PredictionModelSerializer(serializers.ModelSerializer):
     
-    
+    result = PredictionResultSerializer()
     
     class Meta:
         model = PredictionModel
