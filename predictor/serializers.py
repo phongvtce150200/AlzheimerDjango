ecc577786a2c62e6fa292369863a0186d090f3bd
Phong Thanh Vo <v.thanhphong1712@gmail.com>
2023-03-25 18:59:07 +0700

    Fix tiep di

--- predictor/serializers.py
+++ predictor/serializers.py
@@ -3,23 +3,24 @@ from .models import PredictionModel, PredictionResult, PredictionResultItem
 
 
 class PredictionResultItemSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = PredictionResultItem
-        fields = '__all__'
+        fields = "__all__"
+
 
 class PredictionResultSerializer(serializers.ModelSerializer):
-
     items = PredictionResultItemSerializer(many=True)
 
     class Meta:
         model = PredictionResult
-        fields = '__all__'
+        fields = "__all__"
+        read_only_fields = ["items"]
+
 
 class PredictionModelSerializer(serializers.ModelSerializer):
-    
-    result = PredictionResultSerializer()
-    
+    result = serializers.RelatedField()
+
     class Meta:
         model = PredictionModel
-        fields = '__all__'
+        fields = "__all__"
+        read_only_fields = ["result"]
